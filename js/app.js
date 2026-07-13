7b8d0c3d555803bf391be61dd1373ec33378a7d7
t-barrios <cherub.nevermind@gmail.com>
2026-07-13 11:43:11 -0400

    ajuste para ruta relativa en fetch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function getProducts() {
-    const response = await fetch('data/products.json');
+    const response = await fetch('./data/products.json');
     const data = await response.json();
     return data;
 }
